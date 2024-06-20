34e362de0b304c96af9a2a0236c8e75c6f0af399
rube200 <rube200@users.noreply.github.com>
2024-06-20 19:32:01 +0100

    Setup db connection

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mssql:host=localhost;dbname=VS_Yii2',
+    'username' => 'Yii_Demo',
+    'password' => 'Yii_Demo_Password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
