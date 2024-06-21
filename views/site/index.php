b6c7f880ee490710e64bd1d0aa1cf242f243f669
rube200 <rube200@users.noreply.github.com>
2024-06-21 23:47:50 +0100

    Update index to table of tarefas

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,56 @@
 <?php
 
 /** @var yii\web\View $this */
+/** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'My Yii Application';
+use yii\bootstrap5\Html;
+use yii\grid\GridView;
+
+$this->title = 'Tarefas de ' . Yii::$app->user->identity->username;
 ?>
 <div class="site-index">
+    <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
+    <div class="row">
+        <?= GridView::widget([
+            'columns' => [
+                [
+                    'attribute' => 'titulo',
+                    'contentOptions' => ['class' => 'w-auto'],
+                    'format' => 'text',
+                    'label' => 'Título da tarefa',
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
+                ],
+                [
+                    'attribute' => 'descricao',
+                    'contentOptions' => ['class' => 'w-auto'],
+                    'format' => 'text',
+                    'label' => 'Descrição',
+                ],
+                [
+                    'attribute' => 'data_criacao',
+                    'contentOptions' => ['class' => 'w-auto'],
+                    'format' => ['date', 'php: d/m/Y'],
+                    'label' => 'Data de Criação',
+                ],
+                [
+                    'attribute' => 'data_conclusao',
+                    'contentOptions' => ['class' => 'w-auto'],
+                    'format' => ['date', 'php: d/m/Y'],
+                    'label' => 'Data de Conclusão',
+                ],
+                [
+                    'contentOptions' => ['class' => 'w-auto'],
+                    'format' => 'raw',
+                    'label' => 'Estado'
+                ]
+            ],
+            'dataProvider' => $dataProvider,
+            'emptyText' => 'Nenhuma tarefa encontrada',
+            'formatter' => ['class' => 'yii\i18n\Formatter', 'nullDisplay' => ''],
+            'layout' => "{items}\n{pager}",
+            'options' => ['class' => 'border grid-view px-0 rounded'],
+            'tableOptions' => ['class' => 'mb-0 table table-hover table-striped'],
+        ]); ?>
     </div>
 </div>
