4a8f203c3ea5927dda0d7410b51ab2ef1dc63b4e
rube200 <rube200@users.noreply.github.com>
2024-06-21 16:08:23 +0100

    Update db connection driver

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mssql:host=localhost;dbname=VS_Yii2',
+    'dsn' => 'sqlsrv:Server=localhost;Database=VS_Yii2',
     'username' => 'Yii_Demo',
     'password' => 'Yii_Demo_Password',
     'charset' => 'utf8',
--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mssql:host=localhost;dbname=VS_Yii2_Test';
+$db['dsn'] = 'sqlsrv:Server=localhost;Database=VS_Yii2_Test';
 
 return $db;
