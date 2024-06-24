7a0813252bcaec41a92769c6bfe95468ff85276b
rube200 <rube200@users.noreply.github.com>
2024-06-24 17:16:56 +0100

    Update tarefa migration

--- migrations/m240102_000000_create_tarefas_table.php
+++ migrations/m240102_000000_create_tarefas_table.php
@@ -18,7 +18,7 @@ class m240102_000000_create_tarefas_table extends Migration
             'id' => $this->primaryKey(),
             'owner_id' => $this->integer()->notNull(),
             'titulo' => $this->string()->notNull(),
-            'descricao' => $this->string()->notNull(),
+            'descricao' => $this->text()->notNull(),
             'data_criacao' => $this->date()->notNull()->defaultExpression('GETDATE()'),
             'data_conclusao' => $this->date()->null(),
             'estado' => $this->string()->notNull()->check('estado IN(\'Pendente\', \'Em Curso\', \'Finalizado\')')->defaultValue('Pendente'),
