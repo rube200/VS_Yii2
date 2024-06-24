32436f6f2ca76533b87481002976b0184223abb3
rube200 <rube200@users.noreply.github.com>
2024-06-24 18:42:07 +0100

    Adding icons to tarefa view

--- views/tarefa/view.php
+++ views/tarefa/view.php
@@ -10,6 +10,37 @@ $this->title = 'Tarefa: ' . $model->titulo;
 $this->params['breadcrumbs'][] = ['label' => 'Tarefas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 $this->params['should_center'] = true;
+
+function createActionsButtons($model) {
+    $buttons = [
+        Html::a(
+            Html::tag('i', '', ['class' => 'bi bi-eye']) . ' Lista',
+            ['index'],
+            ['class' => 'btn btn-success ms-2']
+        ),
+        Html::a(
+            Html::tag('i', '', ['class' => 'bi bi-pencil']) . ' Editar',
+            ['update', 'id' => $model->id],
+            ['class' => 'btn btn-primary ms-2']
+        ),
+        Html::a(
+            Html::tag('i', '', ['class' => 'bi bi-trash']) . ' Apagar',
+            ['delete', 'id' => $model->id],
+            [
+                'class' => 'btn btn-danger ms-2',
+                'data' => [
+                    'confirm' => 'Quer mesmo apagar esta tarefa?',
+                    'method' => 'post',
+                ]
+            ]
+        )
+    ];
+
+    return Html::tag(
+        'div',
+        implode('', $buttons),
+        ['class' => 'd-flex justify-content-end px-0 py-1']);
+}
 ?>
 <div class="tarefa-view w-50">
     <h1><?= Html::encode($this->title) ?></h1>
@@ -26,14 +57,7 @@ $this->params['should_center'] = true;
         'formatter' => ['class' => 'yii\i18n\Formatter', 'nullDisplay' => '-'],
     ]) ?>
 
-    <div class="d-flex justify-content-end px-0 py-1">
-        <?= Html::a('Editar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Apagar', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Quer mesmo apagar esta tarefa?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </div>
+    <?=
+        createActionsButtons($model);
+    ?>
 </div>
