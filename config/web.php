dbe2b92f27b18f4a913bc63a964ea6708a617fcc
rube200 <rube200@users.noreply.github.com>
2024-06-24 17:13:18 +0100

    Enable json requests

--- config/web.php
+++ config/web.php
@@ -13,8 +13,10 @@ $config = [
     ],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'nB6DQ6r36mJyfP-iWjpbhVyyPaT8Q7lH',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
